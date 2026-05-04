2511e3147be14ed072937b34fe219504cb12769a
NatashaGarin <120326066+NatashaGarin@users.noreply.github.com>
2026-05-04 18:22:54 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Election System</title>
+
+    <!-- Bootstrap CSS (LOCAL) -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body class="center-page">
+
+    <div class="container text-center">
+        <h1 class="mb-4">Online Election System</h1>
+
+        <div class="box-wrapper">
+    <a href="voter/register.php" class="small-box">Register</a>
+    <a href="voter/login.php" class="small-box">Login</a>
+    <a href="results/results.php" class="small-box">View Results</a>
+</div>
+    </div>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
